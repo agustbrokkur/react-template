ffd78b7c5c506a878b9c1b7fdef03e207b9a7706
agustbrokkur <ahagbgaha@gmail.com>
2019-06-27 11:29:41 +0000

    Fixed the setup file

--- react-setup.sh
+++ react-setup.sh
@@ -1,11 +1,16 @@
 if [ $# -ne 1 ]; then
-	echo "There must be a single variable"
-	echo "$0"
-	rm "testan - Copy.sh"
+	echo "There must be a single variable with the app name"
+	echo "Correct format:"
+	echo "./react-setup.sh 'My App Name'"
 else
-	sed -i 's/react-template/$1/g' file.txt
+	sed -i 's/react-template/$1/g' package.json
 	cd public
-	sed -i 's/React App/$1/g' file.txt
+	sed -i 's/React App/$1/g' index.html
 	cd ..
+	cd ..
+	mv react-template $1
+	cd $1
 	npm install
+	rm -rf .git
+	rm "react-setup.sh"
 fi
